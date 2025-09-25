ae21da8c982acace14af72ecb8b51ee0c65fdabe
Steven Rivière <stevensteriviere@gmail.com>
2025-09-25 17:55:29 +0200

    update home

--- Pages/Home/Index.cshtml.cs
+++ Pages/Home/Index.cshtml.cs
@@ -5,13 +5,6 @@ namespace Portfolio.Pages.Home
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
 
