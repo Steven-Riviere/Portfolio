4b8fd6f979cf3efbfb39ea1cb51016f8e3e495d1
Steven Rivière <stevensteriviere@gmail.com>
2025-09-29 18:07:15 +0200

    form contacts

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// contact.js
+emailjs.init("ph0xpDeI3pUmYLAn6");
 
-// Write your JavaScript code.
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const statusMsg = document.getElementById('statusMessage');
+
+    emailjs.send("service_paiqvxq", "template_xrtvqar", {
+        user_name: document.getElementById('name').value,
+        user_email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+        time: new Date().toLocaleString()
+    })
+
+        .then(function (response) {
+            statusMsg.textContent = "Message envoyé avec succès !";
+            statusMsg.style.color = "green";
+            document.getElementById('contactForm').reset();
+        }, function (error) {
+            statusMsg.textContent = "Erreur lors de l'envoi. Réessayez plus tard.";
+            statusMsg.style.color = "red";
+        });
+});
+
